f3f52025d1e5cac66fc9e07e6e892443f83f65cb
Raj Davande <raj.davande@gmail.com>
2023-07-03 20:45:36 +0530

    added the function in functions.js

--- functions.js
+++ functions.js
@@ -3,3 +3,8 @@ function showmessagefromfunciton(pol)
     alert("This is obv not my first time here, but from external js "+pol);
     
 }
+function valueofre()
+                {
+                    let n=123.456789;
+                    console.log(n.valueOf());
+                }
