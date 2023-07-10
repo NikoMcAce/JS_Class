74d138da99ddabe2783b62eacb1927e8fd689d4b
Raj Davande <raj.davande@gmail.com>
2023-07-10 21:39:08 +0530

    Added the export function

---
+++ export_function.js
@@ -0,0 +1,8 @@
+const mycred = () => 
+{
+    let Username="Raj Davande";
+    let Password = "911"
+    let text= "This function is called from export_function.js and it says "+Username + " / "+ Password;
+    return text;
+};
+export default mycred;
