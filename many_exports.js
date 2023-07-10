ccc1ed7116c40cc2c508f2b1d510082db8625244
Raj Davande <raj.davande@gmail.com>
2023-07-10 21:53:58 +0530

    Added more imports

---
+++ many_exports.js
@@ -0,0 +1,19 @@
+export const greet = () => 
+{
+    let Username="Raj Davande";
+    let Password = "NikoMcAce"
+    let text= "Hello Im "+Username + " / "+ Password;
+    return text;
+};
+
+export function sum1(n1,n2)
+{
+    sum1=n1+n2;
+    return ("Sum is : "+ sum1);
+}
+
+export const device={
+    name:"Macbook",
+    model:"Air",
+    year:2021
+}
